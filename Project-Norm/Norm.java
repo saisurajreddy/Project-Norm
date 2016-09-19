052fe223cadaa31bb22e748c332570249ee14d81
saisurajreddy <d.saisurajreddy@gmail.com>
2016-09-19 18:50:36 +0530

    second commit

--- Project-Norm/Norm.java
+++ Project-Norm/Norm.java
@@ -1,3 +1,14 @@
+/*
+this is a java program that takes Relationship, no. of functional dependencies and we need
+scan all those functional dependencies line by line.
+output will be guessing regarding candidate key , first normal for, second normal form and third
+normal form was done.
+
+created by Sai Suraj Reddy Doma 
+ 
+*/
+
+
 import java.util.*;
 
 
